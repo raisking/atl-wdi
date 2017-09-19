b22ea6da5b85d9fe67cd12537a5189a51687fb45
raisking <raiprakas007@gmail.com>
2017-09-19 17:56:24 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -3,9 +3,33 @@
 var express = require('express');
 //save an express module as 'app'
 var app = express();
+
+
+
+app.get('/', (req, res) => {
+    console.log(req);  
+    res.send("Welcome to the Pizza Express!");
+  })
+  
+  app.get('/topping/:type', (req, res) => {
+        console.log(req.params);
+        res.send(`${req.params.type} pizza! Good choice.`);
+    });
+/*Make a GET route for /order that has 2 variable destinations 
+(# of pizza, pizza size). When a user visits this route on 
+localhost:3000/order/10/medium, they will see "Your order for 
+10 medium pizzas will be ready in 1 minute!" The syntax for the 
+route will look like this:
+*/
+  
+app.get('/order/:amount/:size', (req, res) => {
+        console.log(req.params);
+         res.send(`Your order for ${req.params.amount}, ${req.params.size} pizzas will be ready in 1 minute!` );
+     });
+
 // assigning 3000 as our port
 var port = 3000;
-
+            
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")
